c30dae9396a353bb90f18ccaee64a150d3777241
root <master.zion@gmail.com>
2018-06-01 22:21:24 +0200

    design improved, code cleanup, new logo

--- web/js/login.js
+++ web/js/login.js
@@ -1,4 +1,4 @@
 function DoSubmit(){
-  document.passform.myinput.value = md5(document.getElementById("pwd").value+salt);
+  document.passform.password.value = md5(document.getElementById("pwd").value+salt);
   document.getElementById("myform").submit();
 }
