f85cedcb221bba74ee02baca4830b444d9abdb0d
root <master.zion@gmail.com>
2018-12-27 17:04:31 +0100

    added more time interval to detect the milight controller

--- modules/milight/light_manager.sh
+++ modules/milight/light_manager.sh
@@ -6,6 +6,7 @@ source ~/.bashrc
 MILIGHT_MAC=$(cat milight_port.conf | grep 'MAC'  | awk '{print $2}')
 MILIGHT_PORT=$(cat milight_port.conf | grep 'PORT'  | awk '{print $2}')
 
+MILIGHT_IP="$(arp-scan --localnet | grep $MILIGHT_MAC  | awk ' { printf $1 } ')"
 #wait for the milight box startup
 until [ ! -z "$MILIGHT_IP"  ]; do
   MILIGHT_IP="$(arp-scan --localnet | grep $MILIGHT_MAC  | awk ' { printf $1 } ')"
