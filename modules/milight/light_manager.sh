59804eeb3274cf59a0883ab04b29071bba7b89a7
root <master.zion@gmail.com>
2018-12-27 17:01:48 +0100

    added more time interval to detect the milight controller

--- modules/milight/light_manager.sh
+++ modules/milight/light_manager.sh
@@ -9,7 +9,7 @@ MILIGHT_PORT=$(cat milight_port.conf | grep 'PORT'  | awk '{print $2}')
 #wait for the milight box startup
 until [ ! -z "$MILIGHT_IP"  ]; do
   MILIGHT_IP="$(arp-scan --localnet | grep $MILIGHT_MAC  | awk ' { printf $1 } ')"
-  sleep 1
+  sleep 10
 done
 
 ./light_manager.py $SMARTHOME_MEMDB_PORT $(cat modulename.txt) $MILIGHT_IP $MILIGHT_PORT
