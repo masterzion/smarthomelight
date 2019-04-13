bc17a8fdbe94caea8af29d143f55361342c5e2ff
root <master.zion@gmail.com>
2019-04-13 21:36:48 +0200

    added switch_group_color, bug fixes

--- modules/houseisempty/mobile_check.sh
+++ modules/houseisempty/mobile_check.sh
@@ -9,7 +9,7 @@ MAX_RETRY=50
 MODULENAME=$(cat modulename.txt)
 ITEMNAME="mobile_check"
 
-$SMARTHOME_DIR/bin/memdb_client.py 3030 S VALUES $MODULENAME $ITEMNAME 1 > /dev/null
+$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME $ITEMNAME 1 > /dev/null
 
 get_state() {
     IP=$1
@@ -44,14 +44,14 @@ do
     if [ $ISEMPT -eq 0 ]; then
         sleep 5
         if [ $RETRY -eq $MAX_RETRY ]; then
-           $SMARTHOME_DIR/bin/memdb_client.py 3030 S VALUES $MODULENAME $ITEMNAME 1 > /dev/null
+           $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME $ITEMNAME 1 > /dev/null
            $GPIO -g write $AIR_VICK_GPIO 1
         else
 #            echo "Retry $RETRY"
             RETRY=$((RETRY+1))
         fi
     else
-        $SMARTHOME_DIR/bin/memdb_client.py 3030 S VALUES $MODULENAME $ITEMNAME 0 > /dev/null
+        $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME $ITEMNAME 0 > /dev/null
         sleep 60
         RETRY=1
     fi
--- modules/weatherstation/getday.sh
+++ modules/weatherstation/getday.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 source ~/.bashrc
+MODULENAME=$(cat modulename.txt)
+ITEMNAME="getday"
 
 while true;
 do
    ./getday.py > $SMARTHOME_DIR/modules/webserver/home/js/day.json
    sleep 120
 done
+
