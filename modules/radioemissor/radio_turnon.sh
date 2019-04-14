e042d98a79bf40cfa1568400be6eb0ad1fe998ae
root <master.zion@gmail.com>
2019-04-14 12:30:19 +0200

    remove noise

--- modules/radioemissor/radio_turnon.sh
+++ modules/radioemissor/radio_turnon.sh
@@ -5,4 +5,6 @@ source ~/.bashrc
 MODULE_NAME=$(cat modulename.txt)
 MODULE_ITEM="radio_turnon"
 
+$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS sound sound_turnon 0 > /dev/null
+sleep 5
 $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS $MODULE_NAME $MODULE_ITEM -1 > /dev/null
