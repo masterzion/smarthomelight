5d93729c19da52c0a2fcf2922bdfaceea247797c
root <master.zion@gmail.com>
2019-04-14 12:15:59 +0200

    radio missor

--- modules/sound/auto_sound.sh
+++ modules/sound/auto_sound.sh
@@ -8,6 +8,7 @@ MODULE_ITEM="sound_turnon"
 
 CINEMAMODE_STRING="cinemamode cinemamode_on"
 PLAYMUSIC_STRING="music play"
+RADIOEMISSOR_STRING="radioemissor radio_turnon"
 
 
 $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULE_NAME $MODULE_ITEM 1 > /dev/null
@@ -15,26 +16,35 @@ $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULE_NAME $
 
 while true;
 do
-    CINEMAMODE=$($SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT G PIDS $CINEMAMODE_STRING  )
-    PLAY_MUSIC=$($SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT G PIDS $PLAYMUSIC_STRING  )
-
-    if [ "$CINEMAMODE" == "" ] ; then
-        CINEMAMODE=0
+    RADIOEMISSOR=$($SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT G PIDS $RADIOEMISSOR_STRING  )
+    if [ "$RADIOEMISSOR" == "" ] ; then 
+        RADIOEMISSOR=0
     fi
 
+    if [ "$RADIOEMISSOR" == "0" ] ; then
+	    CINEMAMODE=$($SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT G PIDS $CINEMAMODE_STRING  )
+	    PLAY_MUSIC=$($SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT G PIDS $PLAYMUSIC_STRING  )
 
-    if [ "$PLAY_MUSIC" == "" ] ; then 
-        PLAY_MUSIC=0
-    fi
+	    if [ "$CINEMAMODE" == "" ] ; then
+	        CINEMAMODE=0
+	    fi
 
-    ACTIVE="$CINEMAMODE$PLAY_MUSIC"
+	    if [ "$PLAY_MUSIC" == "" ] ; then 
+	        PLAY_MUSIC=0
+	    fi
 
-    # house is empt
-    if [ "$ACTIVE" == "00" ] ; then
-       $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS $MODULE_NAME $MODULE_ITEM 0 > /dev/null
+	    ACTIVE="$CINEMAMODE$PLAY_MUSIC"
+
+	    # house is empt
+	    if [ "$ACTIVE" == "00" ] ; then
+	       $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS $MODULE_NAME $MODULE_ITEM 0 > /dev/null
+	    else
+	       $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS $MODULE_NAME $MODULE_ITEM -1 > /dev/null
+	    fi
     else
-       $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS $MODULE_NAME $MODULE_ITEM -1 > /dev/null
+	$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS $MODULE_NAME $MODULE_ITEM 0 > /dev/null
     fi
 
     sleep 20
 done
+
