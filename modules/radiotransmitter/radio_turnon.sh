07bfeab602c92508031815a4293e7c23c9bbd754
root <master.zion@gmail.com>
2019-12-24 18:37:50 +0100

    must disable radio to not consume all battery

--- modules/radiotransmitter/radio_turnon.sh
+++ modules/radiotransmitter/radio_turnon.sh
@@ -6,5 +6,5 @@ MODULE_NAME=$(cat modulename.txt)
 MODULE_ITEM="radio_turnon"
 
 $SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS sound sound_turnon 0 > /dev/null
-#sleep 2400
-#$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS $MODULE_NAME $MODULE_ITEM 0 > /dev/null
+sleep 4800
+$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S PIDS $MODULE_NAME $MODULE_ITEM 0 > /dev/null
