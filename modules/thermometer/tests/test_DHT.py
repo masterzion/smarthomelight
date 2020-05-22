0d09b9e75078e226b75bb64c2ac81b52e41182cb
Master Zion <master.zion@gmail.com>
2020-05-22 22:34:13 +0200

    fixed date in test

--- modules/thermometer/tests/test_DHT.py
+++ modules/thermometer/tests/test_DHT.py
@@ -11,6 +11,6 @@ while True:
     if isinstance(sensor2_value_t, float):
       print sensor2_value_h, sensor2_value_t
     else:
-      current_time = now.strftime("%H:%M:%S")
+      current_time = datetime.now().strftime("%H:%M:%S")
       print("Error: ", current_time)
     time.sleep(2)
