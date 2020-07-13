ffc1943df9b12545792018a68bd3390dc4b1acdb
Master Zion <master.zion@gmail.com>
2020-07-13 22:37:35 +0200

    bug fixes in milight

--- modules/milight/light_manager.sh
+++ modules/milight/light_manager.sh
@@ -2,6 +2,7 @@
 
 source ~/.smarthomelight
 
+MODULENAME=$(cat modulename.txt)
 export MILIGHT_MAC=$(echo "$MILIGHT_MAC" | tr '[:upper:]' '[:lower:]')
 
 MILIGHT_IP="$(sudo /usr/sbin/arp-scan --localnet --interface=$IFACE | grep $MILIGHT_MAC  | awk ' { printf $1 } ')"
@@ -13,10 +14,10 @@ done
 
 
 
-$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME switch_group1_color ff8800
-$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME switch_group2_color ff8800
-$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME switch_group3_color ff8800
-$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME switch_group4_color ff8800
+$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME switch_group1_color ff8800 > /dev/null
+$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME switch_group2_color ff8800 > /dev/null
+$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME switch_group3_color ff8800 > /dev/null
+$SMARTHOME_DIR/bin/memdb_client.py $SMARTHOME_MEMDB_PORT S VALUES $MODULENAME switch_group4_color ff8800 > /dev/null
 
 
 ./light_manager.py $SMARTHOME_MEMDB_PORT $(cat modulename.txt) $MILIGHT_IP $MILIGHT_PORT
